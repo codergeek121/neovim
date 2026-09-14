ca3c9e135bae01e9f6bc9fa39339029562cf0404
Niklas Haeusele <niklas.haeusele@hey.com>
2026-09-14 23:56:46 +0200

    Add mergetool binding

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,4 @@
 vim.pack.add { gh('tpope/vim-fugitive') }
 
 vim.keymap.set('n', '<leader>gs', ':vertical Git<CR>', { desc = 'Git status' })
+vim.keymap.set('n', '<leader>gm', ':Git mergetool<CR>', { desc = 'Git mergetool (merge conflicts)' })
